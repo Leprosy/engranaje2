35660cc46a03690cb9a6caa0b5d5266596a9270f
mrojas <leprosy57@gmail.com>
2013-01-22 17:35:54 -0300

    SQL

--- server/eng2.sql
+++ server/eng2.sql
@@ -1,30 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 3.5.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 10, 2012 at 11:28 AM
--- Server version: 5.5.28-0ubuntu0.12.04.2
--- PHP Version: 5.3.10-1ubuntu3.4
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `eng2`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `node`
---
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_id` bigint(20) unsigned NOT NULL,
+  `node_id` bigint(20) unsigned NOT NULL,
+  `user` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `mail` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `content` text COLLATE utf8_spanish_ci NOT NULL,
+  `status` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `karma` smallint(6) NOT NULL,
+  `published_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+INSERT INTO `comment` (`id`, `comment_id`, `node_id`, `user`, `mail`, `content`, `status`, `karma`, `published_at`) VALUES
+(1, 0, 4, 'Miguel', 'leprosy57@gmail.com', 'This is a comment', 'published', 3, '2013-01-10 11:06:00'),
+(2, 1, 4, 'Leprosy', 'miguel@nimbic.com', 'crap comments', 'spam', -4, '2013-01-10 12:06:00');
 
 CREATE TABLE IF NOT EXISTS `node` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -40,34 +31,24 @@ CREATE TABLE IF NOT EXISTS `node` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `slug` (`slug`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `node`
---
-
 INSERT INTO `node` (`id`, `user_id`, `status`, `title`, `slug`, `description`, `content`, `created_at`, `published_at`, `modified_at`) VALUES
-(1, 1, 'published', 'First one', 'first-one', 'First one in db', 'THis is First one in db', '2012-12-10 00:00:00', '2012-12-10 04:00:00', '0000-00-00 00:00:00'),
-(2, 1, 'published', 'TOW', 'tow', '2nd one in db', 'THis is 2nd one in db', '2012-12-10 00:00:00', '2012-12-10 14:00:00', '0000-00-00 00:00:00'),
-(3, 1, 'published', 'This is Sparta', 'this-is-sparta', '3rd one in db', '33333333333333THis is First one in db', '2012-12-10 00:00:00', '2012-12-10 16:30:00', '0000-00-00 00:00:00'),
-(4, 1, 'published', 'Tool', 'tttow', 'sdfsdrwe efvre v wervtr', 'sdfSDTHis is 2nd one in dbFSDTHis is 2nd one in dbFDSTHis is 2nd one in dbFSDTHis is 2nd one in dbFTHis is 2nd one in dbSDFsd', '2012-12-10 00:00:00', '2012-12-10 18:10:00', '0000-00-00 00:00:00');
+(1, 1, 'published', 'This is an amazing postss', 'first-one', 'First one in db. Another one, amazing posts. This funny post, nedd moar posts. Nom nom nom.', 'THis is First one in db', '2012-12-10 00:00:00', '2012-12-10 04:00:00', '0000-00-00 00:00:00'),
+(2, 1, 'published', 'Another one, this time, is the second', 'tow', 'Another one, amazing posts. This funny post, nedd moar posts. Nom nom nom.', 'THis is 2nd one in db', '2012-12-10 00:00:00', '2012-12-10 14:00:00', '0000-00-00 00:00:00'),
+(3, 1, 'draft', 'This is Sparta', 'this-is-sparta', 'Another one, amazing posts .First one in db. This funny post, nedd moar posts. Nom nom nom.', '33333333333333THis is First one in db', '2012-12-10 00:00:00', '2012-12-10 14:30:00', '0000-00-00 00:00:00'),
+(4, 1, 'published', 'Tool', 'tttow', 'First one in db. Another one, amazing posts. This funny post, nedd moar posts. Nom nom nom.', '<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that''s what you see at a toy store. And you must think you''re in a toy store, because you''re here shopping for an infant named Jeb. </p>\n\n<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they''re actually proud of that shit.  </p>\n\n<p>Now that we know who you are, I know who I am. I''m not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain''s going to be? He''s the exact opposite of the hero. And most times they''re friends, like you and me! I should''ve known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>', '2012-12-10 00:00:00', '2012-12-10 14:40:00', '0000-00-00 00:00:00');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `node_term`
---
 
 CREATE TABLE IF NOT EXISTS `node_term` (
-  `id` bigint(20) unsigned NOT NULL,
   `node_id` bigint(20) unsigned NOT NULL,
   `term_id` bigint(20) unsigned NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `term`
---
+INSERT INTO `node_term` (`node_id`, `term_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2),
+(4, 3),
+(4, 1);
 
 CREATE TABLE IF NOT EXISTS `term` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -76,16 +57,8 @@ CREATE TABLE IF NOT EXISTS `term` (
   `type` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `term`
---
-
 INSERT INTO `term` (`id`, `name`, `slug`, `type`) VALUES
 (1, 'Tecnología', 'tecnologia', 'category'),
 (2, 'Música', 'musica', 'category'),
 (3, 'Android', 'android', 'tag');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
