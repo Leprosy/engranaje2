2912d693398cdb41c8b968c02b1e444fe43bfab2
mrojas <leprosy57@gmail.com>
2013-01-31 14:31:00 -0300

    Final SQL

--- server/eng2.sql
+++ server/eng2.sql
@@ -12,15 +12,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `karma` smallint(6) NOT NULL,
   `published_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=7 ;
-
-INSERT INTO `comment` (`id`, `comment_id`, `node_id`, `user`, `mail`, `content`, `status`, `karma`, `published_at`) VALUES
-(1, 0, 4, 'Miguel', 'leprosy57@gmail.com', 'This is a comment', 'published', 3, '2013-01-10 11:06:00'),
-(2, 1, 4, 'Leprosy', 'miguel@nimbic.com', 'crap comments', 'spam', -4, '2013-01-10 12:06:00'),
-(3, 0, 23, 'basdasdf', 'asdfsdaf', 'dfwer234 234 234234', 'published', 0, '2013-01-30 09:48:29'),
-(4, 0, 4, 'BLAST', 'fuck@you.com', 'FUCK YOU', 'published', 0, '2013-01-30 09:52:47'),
-(5, 0, 4, 'BLAST', 'fuck@you.com', 'FUCK YOU', 'published', 0, '2013-01-30 09:53:03'),
-(6, 0, 4, 'BLAST', 'fuck@you.com', 'FUCK YOU', 'published', 0, '2013-01-30 09:53:04');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `media` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -36,7 +28,10 @@ CREATE TABLE IF NOT EXISTS `media` (
   `modified_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `slug` (`slug`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=2 ;
+
+INSERT INTO `media` (`id`, `user_id`, `status`, `type`, `title`, `slug`, `description`, `content`, `created_at`, `published_at`, `modified_at`) VALUES
+(1, 1, 'published', 'image', 'El inicio', 'el-inicio', 'Primera foto', '3.nocturno.jpg', '2013-01-31 13:42:35', '2013-01-31 13:42:35', '2013-01-31 13:42:35');
 
 CREATE TABLE IF NOT EXISTS `node` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -52,15 +47,10 @@ CREATE TABLE IF NOT EXISTS `node` (
   `modified_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `slug` (`slug`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=25 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=2 ;
 
 INSERT INTO `node` (`id`, `user_id`, `status`, `title`, `slug`, `description`, `media`, `content`, `created_at`, `published_at`, `modified_at`) VALUES
-(1, 1, 'published', 'This is an amazing postss', 'first-one', 'First one in db. Another one, amazing posts. This funny post, nedd moar posts. Nom nom nom.', '', 'THis is First one in db', '2012-12-10 00:00:00', '2012-12-10 04:00:00', '0000-00-00 00:00:00'),
-(2, 1, 'published', 'Another one, this time, is the second', 'tow', 'Another one, amazing posts. This funny post, nedd moar posts. Nom nom nom.', '', 'THis is 2nd one in db', '2012-12-10 00:00:00', '2012-12-10 14:00:00', '0000-00-00 00:00:00'),
-(3, 1, 'draft', 'This is Sparta', 'this-is-sparta', 'Another one, amazing posts .First one in db. This funny post, nedd moar posts. Nom nom nom.', '', '33333333333333THis is First one in db', '2012-12-10 00:00:00', '2012-12-10 14:30:00', '0000-00-00 00:00:00'),
-(4, 1, 'published', 'Tool', 'tttow', 'First one in db. Another one, amazing posts. This funny post, nedd moar posts. Nom nom nom.', '', '<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that''s what you see at a toy store. And you must think you''re in a toy store, because you''re here shopping for an infant named Jeb. </p>\n\n<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they''re actually proud of that shit.  </p>\n\n<p>Now that we know who you are, I know who I am. I''m not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain''s going to be? He''s the exact opposite of the hero. And most times they''re friends, like you and me! I should''ve known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>', '2012-12-10 00:00:00', '2012-12-10 14:40:00', '0000-00-00 00:00:00'),
-(23, 1, 'published', 'Con 100 mil ñandúes', 'con-100-mil-nandues', 'this is descriptive', '', 'blasdvasdfds', '2013-01-30 09:47:14', '2013-01-30 09:47:14', '2013-01-30 09:47:14'),
-(24, 1, 'published', 'Con 100 mil ñandúes', 'con-100-mil-nandues-#', 'this is descriptive', '', 'blasdvasdfds', '2013-01-30 09:47:45', '2013-01-30 09:47:45', '2013-01-30 09:47:45');
+(1, 1, 'published', 'El inicio', 'el-inicio', 'Mi primer post. Inauguro este nuevo espacio, que a la vez es un invento/proyecto...', 'el-inicio', 'El primer post. Wow...', '2013-01-31 13:34:09', '2013-01-31 13:34:09', '2013-01-31 13:34:09');
 
 CREATE TABLE IF NOT EXISTS `node_term` (
   `node_id` bigint(20) unsigned NOT NULL,
