4125f19cb95dd95196a164f29edd067d5add17e6
mrojas <leprosy57@gmail.com>
2013-01-31 13:13:25 -0300

    Greatest addition ever : pretty urls for server

--- server/index.php
+++ server/index.php
@@ -19,14 +19,23 @@ spl_autoload_register(array('Autoloader', 'loadClass'));
 
 /* Front controller */
 try {
-    $module = isset($_GET['module']) ? $_GET['module'] : null;
+    if (!isset($_GET['url'])) {
+        $_GET['url'] = null;
+    }
+
+    $url = explode('/', $_GET['url']);
+    $module = isset($url[0]) ? $url[0] : null;
 
     if ($module == null) {
         throw new Exception('module_not_found ' . $module);
     }
 
-    $action = strtolower(ENG_REQUEST_METHOD) . '_' .
-              (isset($_GET['action']) ? $_GET['action'] : 'index');
+    $action = strtolower(ENG_REQUEST_METHOD) . '_' . (isset($url[1]) ? $url[1] : 'index');
+
+    /* ids/slugs/keys */
+    if (isset($url[2])) {
+        $_GET['id'] = $url[2];
+    }
 
     if (class_exists($module, true)) {
         $M = new $module();
