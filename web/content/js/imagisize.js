8b2be516e8c1f5ac85c7d8aed7e0ea8748130c42
mrojas <leprosy57@gmail.com>
2013-02-04 13:04:20 -0300

    Imagisize library

--- web/content/js/imagisize.js
+++ web/content/js/imagisize.js
@@ -1,11 +1,29 @@
 var Imagisize = {
+    sel: false,
+    sizes: false,
     init: function(sel, sizes) {
+        Imagisize.sel = sel;
+        Imagisize.sizes = sizes;
         var size = $(document).width();
 
-        $(sel + ' img').each(function(a, b) {
-            for (var s in sizes) {
-                if (size >= sizes[s]) {
-                    b.setAttribute('src', b.getAttribute('res-' + sizes[s]));
+        Imagisize.doResize(size);
+
+        $(window).on('resize', function() {
+            var size = $(document).width();
+            console.log('Imagisize : Size changed to ' + size)
+        });
+
+        console.log('Imagisize : library initialized');
+    },
+
+    doResize: function(size) {
+        $(Imagisize.sel + ' img').each(function(a, b) {
+            for (var s in Imagisize.sizes) {
+                if (size >= Imagisize.sizes[s]) {
+                    var file = b.getAttribute('res-' + Imagisize.sizes[s]);
+                    if (file) {
+                        b.setAttribute('src', file);
+                    }
                 }
             }
         });
