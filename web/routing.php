9a026a49fcb7e784e3be0765941f4907d9f263eb
mrojas <leprosy57@gmail.com>
2013-02-08 10:24:52 -0300

    Routing rules

---
+++ web/routing.php
@@ -0,0 +1,25 @@
+<?php
+/* Routes */
+$_engRoutes = array(
+        '/^([0-9]+)\/([0-9]+)\/([a-z0-9\-]+)/' =>
+        array(
+                'action' => 'post',
+                'params' => array('year', 'month', 'slug')
+        ),
+
+        '/^term\/([a-z0-9\-]+)/' =>
+        array(
+                'action' => 'term',
+                'params' => array('term')
+        ),
+
+        '/^comment/' =>
+        array(
+                'action' => 'comment'
+        ),
+
+        '/^$/' =>
+        array(
+                'action' => 'home'
+        )
+);
