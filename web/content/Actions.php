d02438a6800f108610f5c1d4b42a96543a92be26
mrojas <leprosy57@gmail.com>
2013-02-08 12:13:33 -0300

    Error 404

--- web/content/Actions.php
+++ web/content/Actions.php
@@ -2,5 +2,9 @@
 class Actions extends BaseActions {
     function home($data) {
         $this->posts = self::getRequest('node?limit=6');
+
+        if (isset($this->posts->http_code)) {
+            $this->forwardTo('error404', $data);
+        }
     }
 }
